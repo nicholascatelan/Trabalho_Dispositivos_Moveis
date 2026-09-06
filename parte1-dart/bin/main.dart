fe6fff402149b69ba3d90c7235cbad266a18a62d
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-09-06 15:56:20 -0300

    feat: implementa herança entre instrumentos

--- parte1-dart/bin/main.dart
+++ parte1-dart/bin/main.dart
@@ -1,6 +1,12 @@
-import 'modelos/instrumento.dart';
+import 'models/instrumento.dart';
+import 'models/instrumento_eletrico.dart';
+
 
 void main() {
+  // ==================================================
+  // EXERCÍCIO 2 — MODELAGEM DA ENTIDADE PRINCIPAL
+  // ==================================================
+
   final instrumento = Instrumento(
     nome: 'Violão Acústico',
     marca: 'Yamaha',
@@ -10,4 +16,27 @@ void main() {
 
   print('===== [1] ENTIDADE PRINCIPAL =====');
   print(instrumento.ficha());
+
+  // ==================================================
+  // EXERCÍCIO 2 - HERANÇA
+  // ==================================================
+
+  // Cria um objeto da classe filha.
+  final instrumentoEletrico = InstrumentoEletrico(
+    nome: 'Guitarra Elétrica',
+    marca: 'Fender',
+    preco: 4500.00,
+    ano: 2025,
+    tipoCaptador: 'Humbucker',
+    potencia: 50,
+  );
+
+  
+  print('\n===== [2] HERANÇA =====');
+
+  
+  print(instrumento.ficha());
+
+  
+  print(instrumentoEletrico.ficha());
 }
