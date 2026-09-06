208625b14e5f39f37eb6b640d32d1e125316ee12
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-09-06 15:52:23 -0300

    comentario_ex2

--- parte1-dart/bin/modelos/instrumento_eletrico.dart
+++ parte1-dart/bin/modelos/instrumento_eletrico.dart
@@ -1,28 +1,24 @@
 import 'instrumento.dart';
 
-
 class InstrumentoEletrico extends Instrumento {
-
   String tipoCaptador;
   double potencia;
 
-
+  // Construtor da classe filha
   InstrumentoEletrico({
-  
+    // super repassa os atributos herdados para a classe pai.
     required super.nome,
     required super.marca,
     required super.preco,
     required super.ano,
 
-  
     required this.tipoCaptador,
     required this.potencia,
   });
 
-
+  // Sobrescreve o método ficha() que existe na classe pai
   @override
   String ficha() {
-
     return '${super.ficha()} | Captador: $tipoCaptador | Potência: ${potencia}W';
   }
 }
