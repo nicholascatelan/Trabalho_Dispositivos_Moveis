e927056c9395d4d42f03e13d560f4265fbc0cc0b
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-09-12 19:09:10 -0300

    feat: fazendo  exercicio5

--- parte2_flutter/lib/screens/home_page.dart
+++ parte2_flutter/lib/screens/home_page.dart
@@ -6,8 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Barra superior
-      appBar: AppBar(  
+
+      // Barra superior da tela
+      appBar: AppBar(
         title: const Text(
           'Loja de Instrumentos',
           style: TextStyle(
@@ -19,9 +20,53 @@ class HomePage extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
 
-    
-    
-    
+      // Corpo da tela
+      body: Column(
+
+        // Controla o eixo vertical
+        mainAxisAlignment: MainAxisAlignment.start,
+
+        // Controla o eixo horizontal
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children: [
+
+          // Total do Exercício 4
+          Container(
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(20),
+
+            decoration: BoxDecoration(
+              color: Colors.indigo,
+              borderRadius: BorderRadius.circular(16),
+            ),
+
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Valor total do catálogo',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15,
+                  ),
+                ),
+
+                SizedBox(height: 5),
+
+                Text(
+                  'R\$ 15.199,80',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
