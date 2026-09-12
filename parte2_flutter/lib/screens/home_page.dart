aebe1cf86957ade2c384ae6a6c9d95e7671e1513
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-09-12 18:13:53 -0300

    feat: criando  barra superior da tela

--- parte2_flutter/lib/screens/home_page.dart
+++ parte2_flutter/lib/screens/home_page.dart
@@ -6,50 +6,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Loja de Instrumentos'),
+      // Barra superior
+      appBar: AppBar(  
+        title: const Text(
+          'Loja de Instrumentos',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              'Valor total: R\$ 15.199,80',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          Expanded(
-            child: ListView(
-              children: const [
-                ListTile(
-                  title: Text('Violão Acústico'),
-                  subtitle: Text('Yamaha - R\$ 1.299,90'),
-                ),
-                ListTile(
-                  title: Text('Baixo'),
-                  subtitle: Text('Tagima - R\$ 1.899,90'),
-                ),
-                ListTile(
-                  title: Text('Bateria'),
-                  subtitle: Text('Pearl - R\$ 5.200,00'),
-                ),
-                ListTile(
-                  title: Text('Bateria Eletrônica'),
-                  subtitle: Text('Roland - R\$ 6.800,00'),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    
+    
+    
     );
   }
 }
